e1b3b73f5a6c91bfbd204c1c9e6bb033f2d6a39f
seyon1988 <seyon1988@gmail.com>
2024-12-28 16:11:25 +0530

    Chapter folders created

---
+++ folder_creator.py
@@ -0,0 +1,21 @@
+import os
+
+
+input_file_path = './Chapters.txt'
+output_directory = '.'
+
+
+os.makedirs(output_directory, exist_ok=True)
+
+
+with open(input_file_path, 'r') as file:
+    lines = file.readlines()
+
+
+for index, line in enumerate(lines, start=1):
+    line = line.strip()
+    folder_name = f"Chapter_{index:02}_{line}"
+    output_folder_path = os.path.join(output_directory, folder_name)
+    
+    os.makedirs(output_folder_path, exist_ok=True)
+
